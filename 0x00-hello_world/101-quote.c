0f33575727874a61a001f00ab041dae986c7dff7
Youssef <yooooussssef.sameh.a@gmail.com>
2023-08-10 09:53:26 -0700

    ds

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,15 @@
 /**
  * main - entry point
  *
- * Description: A C programe thay prints with fputs function
+ * Description: A C programe thay prints with write function
  *
- * Return: 0
+ * Return: 1
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, c, 59)
 	return (1);
 }
