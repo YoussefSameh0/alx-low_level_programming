62f54abe72fbe1597cf30c7f47a167573abcba03
Youssef <yooooussssef.sameh.a@gmail.com>
2023-08-11 08:56:40 -0700

    cs

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : A C programe that prints with printf function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		printf("%i", i);
+	return (0);
+}
