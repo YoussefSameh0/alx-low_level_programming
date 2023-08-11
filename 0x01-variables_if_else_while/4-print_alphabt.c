7e29b13bf2784760bc7ea58a471dc54aad05560a
Youssef <yooooussssef.sameh.a@gmail.com>
2023-08-11 08:36:56 -0700

    cs

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : A C programe thay prints with putchar function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+	    if (c == 'e' || c == 'q')
+	    {
+		c++;
+		continue;
+	    }
+	    else
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
