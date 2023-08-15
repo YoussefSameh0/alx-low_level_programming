818cad9b3d176b638a4269106d0f98485215fa10
Youssef <yooooussssef.sameh.a@gmail.com>
2023-08-15 06:51:20 -0700

    cs

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description : A C programe that ---
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
